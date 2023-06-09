544eb1f578a14c93f80e0b84a475e877f81f1ee8
ironforester <123802501+ironforester@users.noreply.github.com>
2023-06-09 18:49:00 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,48 @@
+class FlatIterator:
+
+    def __init__(self, list_of_list):
+        self.main_list = list_of_list
+
+    def __iter__(self):
+        self.main_list_cursor = 0
+        self.nested_list_cursor = -1
+        return self
+
+    def __next__(self):
+        self.nested_list_cursor += 1
+        if self.nested_list_cursor == len(self.main_list[self.main_list_cursor]):
+            self.main_list_cursor += 1
+            self.nested_list_cursor = 0
+            if self.main_list_cursor == len(self.main_list):
+                raise StopIteration
+
+        return self.main_list[self.main_list_cursor][self.nested_list_cursor]
+
+list_of_lists_1 = [
+        ['a', 'b', 'c'],
+        ['d', 'e', 'f', 'h', False],
+        [1, 2, None]
+    ]
+print(list(FlatIterator(list_of_lists_1)))
+
+
+def test_1():
+
+    list_of_lists_1 = [
+        ['a', 'b', 'c'],
+        ['d', 'e', 'f', 'h', False],
+        [1, 2, None]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            FlatIterator(list_of_lists_1),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+    ):
+
+        assert flat_iterator_item == check_item
+
+    assert list(FlatIterator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+
+
+if __name__ == '__main__':
+    test_1()
